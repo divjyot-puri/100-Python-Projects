7e7f6cc419a4435b8f4efb2462b5ce990999068d
divjyot-puri <divjyotpuri@gmail.com>
2025-08-16 20:57:08 +0530

    Update 001-Brand_Name_Generator.py

--- 001-Brand_Name_Generator/001-Brand_Name_Generator.py
+++ 001-Brand_Name_Generator/001-Brand_Name_Generator.py
@@ -1,3 +1,9 @@
+"""
+This script generates a band name by combining the user's city and pet names.
+
+The program prompts the user for their city and pet names and then displays a suggested band name.
+"""
+
 print("Welcome to the Band Name Generator")
 print("What's the name of the city you grew up in?")
 city = input("Enter city's name: ")
